7df3b1807bc96601c127d51a92ed0031e7c572f1
Lucas Matheus Rodrigues Brum <lucasmatheusrodriguesbrum@gmail.com>
2021-06-22 09:50:20 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+let botao = document.getElementById('mostrar')
+let modal = document.getElementById('modal')
+botao.addEventListener("click", myFunction);
+function myFunction() {
+  modal.style.display = 'block'
+}
+
